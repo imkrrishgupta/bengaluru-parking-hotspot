0ae52bc78b5858736e6f769ff8e96be37c098d63
Krrish Kumar <imkrrishgupta@gmail.com>
2026-06-21 21:47:08 +0530

    fix: replace BrowserRouter with HashRouter for routing

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import "./utils/fixLeafletIcon";
 import App from "./App.jsx";
 import "./index.css";
@@ -8,8 +8,8 @@ import "leaflet/dist/leaflet.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
